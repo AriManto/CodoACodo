22a3598c1ef21ef06720cfc928f9092649206189
AriManto <37150326+AriManto@users.noreply.github.com>
2018-09-18 17:57:48 -0300

    Fin unidad 1 y 2

---
+++ POO/Unidad 1 y 2/src/herencia/Empleado.java
@@ -0,0 +1,54 @@
+package herencia;
+//Una empresa de informática quiere llevar un registro de todos sus empleados, para eso ha debemos crear las clases correspondientes en Java , las cuales deben incluir lo siguiente :
+//A) Clase Empleado (Clase padre)
+//	Atributos:
+//	- nombre: tipo String (Debe ser nombre y apellido)
+//	- cedula: tipo String
+//	- edad : entero (Rango entre 18 y 45 años)
+//	- casado: boolean
+//	- salario: tipo numérico doble
+//
+//	Métodos a crear en la clase Empleado:
+//
+//	- Constructor  sin parámetros de entrada
+//
+//- Imprimir los datos del empleado por pantalla (utilizar salto de línea \n para separar
+// los atributos.
+
+public class Empleado {
+    private String nombre;
+    private String cedula;
+    private int edad;
+    private boolean casado;
+    private double salario;
+    //Setters y Getters
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public String getCedula() { return cedula;}
+    public void setCedula(String cedula) {this.cedula = cedula;}
+    public int getEdad() {return edad;}
+    public void setEdad(int edad) {
+        if (edad >= 18 && edad <=45) {
+            this.edad = edad;
+        }
+    }
+    public boolean isCasado() {return casado;}
+    public String stringCasado(){
+        if (this.isCasado()){
+            return "Sí";
+        }
+        else {return "No";}
+    }
+    public void setCasado(boolean casado) {this.casado = casado;}
+    public double getSalario() {return salario;}
+    public void setSalario(double salario) {this.salario = salario;}
+    //Constructor
+    public Empleado(){}
+    public void mostrarDatos(){
+        System.out.println("----------------------------------------------");
+        System.out.println("Nombre: "+this.getNombre()
+        +"\nCedula: "+this.getCedula()+"\nEdad: "+this.getEdad()
+        +"\nCasado: "+this.stringCasado()+"\nSalario: "+this.getSalario());
+    }
+
+}
---
+++ POO/Unidad 1 y 2/src/herencia/Programador.java
@@ -0,0 +1,43 @@
+package herencia;
+//2.  Clase Programador (Especialización de Empleado - Clase Hija). Clase que hereda de Empleado todos los atributos y métodos.
+//
+//	Atributos:
+//	- lineasDeCodigoPorHora : tipo entero
+//		- lenguajeDominante: tipo String
+//
+//	Metodos:
+//	- Constructor sin parámetros de entrada.
+public class Programador extends Empleado {
+    private int lineasDeCodigoPorHora;
+    private String lenguajeDominante;
+    //Getters y setters
+    public int getLineasDeCodigoPorHora() {return lineasDeCodigoPorHora;}
+    public void setLineasDeCodigoPorHora(int lineasDeCodigoPorHora){this.lineasDeCodigoPorHora = lineasDeCodigoPorHora;}
+    public String getLenguajeDominante() {return lenguajeDominante;}
+    public void setLenguajeDominante(String lenguajeDominante) {this.lenguajeDominante = lenguajeDominante;}
+
+
+    public Programador(){
+    }
+
+    public static void main(String[] args) {
+        //Carga 1 programador
+        Programador programador1 = new Programador();
+        programador1.setNombre("Fernando Lamas");
+        programador1.setCedula("24E92B-602");
+        programador1.setEdad(25);
+        programador1.setCasado(false);
+        programador1.setSalario(16500);
+        programador1.setLenguajeDominante("Python");
+        programador1.setLineasDeCodigoPorHora(120);
+        //Carga 1 empleado
+        Empleado empleado1 = new Empleado();
+        empleado1.setNombre("Javier García");
+        empleado1.setCedula("27A3C4-507");
+        empleado1.setEdad(42);
+        empleado1.setCasado(true);
+        empleado1.setSalario(18000);
+        programador1.mostrarDatos();
+        empleado1.mostrarDatos();
+    }
+}
